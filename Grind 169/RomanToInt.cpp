9e2299d3c4b2ae5dd8a19a93633a1e7d1872f523
hssn08 <knocker160@gmail.com>
2024-03-03 00:08:48 +0500

    after break

---
+++ Grind 169/RomanToInt.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int nums(char c){
+        if(c == 'I'){
+            return 1;
+        }
+        else if(c == 'V'){
+            return 5;
+        }
+        else if(c == 'X'){
+            return 10;
+        }
+        else if(c == 'L'){
+            return 50;
+        }
+        else if(c == 'C'){
+            return 100;
+        }
+        else if(c == 'D'){
+            return 500;
+        }
+        else {
+            return 1000;
+        }
+    }
+    int romanToInt(string s) {
+        int i = 0, sum = 0;
+        while(i < s.size()-1){
+            if(nums(s[i]) < nums(s[i+1])){
+                sum = sum - nums(s[i]);
+            }
+            else{
+                sum = sum + nums(s[i]);
+            }
+            i++;
+        }
+        sum = sum + nums(s[s.size()-1]);
+        return sum;
+    }
+};
